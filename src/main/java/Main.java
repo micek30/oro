d94988dbd19557c1d409fa85c264f020e84d0a21
micek30 <micek30@gmail.com>
2018-06-25 23:04:37 +0200

    szlify....

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -72,15 +72,6 @@ public class Main {
 
         entityCore.start();
 
-//        while(i < employeesEntities.size() - 1) {
-//            int j = i + 5;
-//            for (; i < j && i < employeesEntities.size() - 1; i++) {
-//                System.out.println(employeesEntities.get(i).toString());
-//            }
-//            System.out.println("1 - dalej");
-//            if (odczyt.nextInt() != 1) i = employeesEntities.size();
-//        }
-
             for (int i=0; i < employeesEntities.size() - 1; i++) {
                 System.out.println(employeesEntities.get(i).toString());
             }
@@ -88,6 +79,25 @@ public class Main {
         entityCore.end();
     }
     private static void filteredSelectEmp(){
+
+
+        System.out.print("Search by name(a) or IdEmployee(b)? ");
+        String choice = odczyt.next();
+
+        while (!"a".equals(choice)&&!"b".equals(choice)){
+            System.out.println("Choice unknown");
+            System.out.print("Do you want search by name(A) or IdEmployee(B)? ");
+            choice = odczyt.next();
+        }
+        if("a".toLowerCase().equals(choice)){
+            selectByName();
+        }else {
+            selectById();
+        }
+
+
+    }
+    private static void selectByName(){
         EntityCore entityCore = new EntityCore();
         entityCore.start();
 
@@ -95,14 +105,37 @@ public class Main {
         String name = odczyt.next();
         List<EmployeesEntity> employeesEntities = new ArrayList<>();
 
-        Query query = entityCore.getEntityManager().createQuery("from EmployeesEntity ");
-        employeesEntities = query.getResultList();
-        for(int i=0;i<employeesEntities.size();i++){
-            if(employeesEntities.get(i).toString().contains(name)){
-                System.out.println(employeesEntities.get(i).toString());
+        try {
+            Query query = entityCore.getEntityManager().createQuery("from EmployeesEntity ");
+            employeesEntities = query.getResultList();
+            for (int i = 0; i < employeesEntities.size(); i++) {
+                if (employeesEntities.get(i).toString().contains(name)) {
+                    System.out.println(employeesEntities.get(i).toString());
+                }
             }
+        }catch (Exception e){
+            System.out.println("Employee not found");
+
+        }finally {
+            entityCore.end();
         }
 
-        entityCore.end();
+    }
+    private static void selectById(){
+        EntityCore entityCore = new EntityCore();
+        entityCore.start();
+
+        System.out.println("Podaj id referencji do pobrania");
+        int idx = odczyt.nextInt();
+        try {
+            EmployeesEntity employeesEntity = entityCore.getEntityManager().getReference(EmployeesEntity.class, idx);
+            System.out.println(employeesEntity.toString());
+        }catch (Exception e){
+            System.out.println("Employee not found");
+        }finally {
+            entityCore.end();
+        }
+
+        //entityCore.end();
     }
 }
